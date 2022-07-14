7ac8cd3cd2a421b4e600de7c28c350204f38a5c3
Ishika1903 <ishahaney1903@gmail.com>
2022-07-14 19:12:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for(item of buttons){ 
+    item.addEventListener('click', (e) => { 
+        buttonText = e.target.innerText;
+        console.log('Button Text is ' , buttonText);
+        if (buttonText =='C'){ 
+            screenValue = "";
+            screen.value = screenValue; 
+        }
+        else if (buttonText =='='){ 
+            screen.value = eval(screen.value);
+        }
+        else{ 
+            screenValue += buttonText;
+            screen.value = screenValue; 
+        }
+   
+    })
+}
